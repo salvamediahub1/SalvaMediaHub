6c533f276da05a1c108d39b4c9728cef3e526f53
salvamediahub1 <salvamediahub1@gmail.com>
2026-08-02 17:18:48 +0100

    Add mobile hamburger menu functionality

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach(el => observer.observe(el));
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
